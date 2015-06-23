772c82ff841920c6b6c3b8a1ed912f06e32761dd
Anthony Franco <anthony_franco@alumni.brown.edu>
2015-06-23 17:51:31 -0400

    added third test to users model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -13,4 +13,10 @@ class UserTest < ActiveSupport::TestCase
     user = User.new(email: "heytodo", username: "htphan", full_name: "Henry Phan", password: "papi")
     refute user.save, "Need a valid email to save"
   end
+
+  test "users should be unique" do
+    user_1 = users(:one)
+    user_2 = users(:two)
+    assert_not_equal(user_1, user_2)
+  end
 end
